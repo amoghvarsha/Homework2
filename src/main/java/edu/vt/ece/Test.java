c59b55992eb4f71f7c0c5871733704b7020c0866
amoghvarsha <amoghvarsha.s@outlook.com>
2024-09-25 11:03:49 -0400

    Updated Test And Test2 to print ARGS

--- src/main/java/edu/vt/ece/Test.java
+++ src/main/java/edu/vt/ece/Test.java
@@ -1,10 +1,12 @@
 package edu.vt.ece;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import edu.vt.ece.bench.Counter;
 import edu.vt.ece.bench.SharedCounter;
 import edu.vt.ece.bench.TestThread;
 import edu.vt.ece.locks.*;
-
 
 public class Test {
 
@@ -14,13 +16,48 @@ public class Test {
     private static final String LOCK_TWO = "LockTwo";
     private static final String PETERSON = "Peterson";
     private static final String FILTER = "Filter";
+    private static final String BAKERY = "Bakery";
 
-    public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
-        String lockClass = (args.length==0 ? PETERSON : args[0]);
-        final Counter counter = new SharedCounter(0, (Lock)Class.forName("edu.vt.ece.locks." + lockClass).newInstance());
-        for(int t=0; t<THREAD_COUNT; t++)
-            new TestThread(counter).start();
+    public static void printArgs(String[] args) {
+        System.out.println("\n************ Command-Line Arguments ************");
+        if (args.length == 0) {
+            System.out.println("No arguments provided.");
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                System.out.println((i + 1) + ". " + args[i]);
+            }
+        }
+        System.out.println("************************************************\n");
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            printArgs(args);
+            String lockClass = (args.length == 0 ? PETERSON : args[0]);
+            int threadCount = (args.length <= 1 ? 2 : Integer.parseInt(args[1]));
+
+            Lock lock;
+
+            if (lockClass.equals(FILTER) || lockClass.equals(BAKERY)) {
+                // Handle locks that require the number of threads as a constructor parameter
+                Class<?> lockClassObj = Class.forName("edu.vt.ece.locks." + lockClass);
+                Constructor<?> lockConstructor = lockClassObj.getDeclaredConstructor(int.class);
+                lock = (Lock) lockConstructor.newInstance(threadCount);
+            } else {
+                // Handle locks that do not require any parameters in the constructor
+                Class<?> lockClassObj = Class.forName("edu.vt.ece.locks." + lockClass);
+                Constructor<?> lockConstructor = lockClassObj.getDeclaredConstructor();
+                lock = (Lock) lockConstructor.newInstance();
+            }
+
+            final Counter counter = new SharedCounter(0, lock);
+            for (int t = 0; t < threadCount; t++) {
+                new TestThread(counter).start();
+            }
+
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
-
-
--- src/main/java/edu/vt/ece/Test2.java
+++ src/main/java/edu/vt/ece/Test2.java
@@ -1,11 +1,12 @@
 package edu.vt.ece;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import edu.vt.ece.bench.Counter;
 import edu.vt.ece.bench.SharedCounter;
-import edu.vt.ece.bench.TestThread;
 import edu.vt.ece.bench.TestThread2;
 import edu.vt.ece.locks.*;
-
 
 public class Test2 {
 
@@ -13,37 +14,75 @@ public class Test2 {
     private static final String LOCK_TWO = "LockTwo";
     private static final String PETERSON = "Peterson";
     private static final String FILTER = "Filter";
+    private static final String BAKERY = "Bakery";
 
-    public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, InterruptedException {
-        String lockClass = (args.length == 0 ? PETERSON : args[0]);
-        int threadCount = (args.length <= 1 ? 16 : Integer.parseInt(args[1]));
-        int totalIters = (args.length <= 2 ? 64000 : Integer.parseInt(args[2]));
-        int iters = totalIters / threadCount;
+    public static void printArgs(String[] args) {
+        System.out.println("\n************ Command-Line Arguments ************");
+        if (args.length == 0) {
+            System.out.println("No arguments provided.");
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                System.out.println((i + 1) + ". " + args[i]);
+            }
+        }
+        System.out.println("************************************************\n");
+    }
 
-        for (int i = 0; i < 2; i++) {
-            run(lockClass, threadCount, iters);
+
+    public static void main(String[] args) {
+
+        try {
+            printArgs(args);
+            String lockClass = (args.length == 0 ? PETERSON : args[0]);
+            int threadCount = (args.length <= 1 ? 16 : Integer.parseInt(args[1]));
+            int totalIters = (args.length <= 2 ? 64000 : Integer.parseInt(args[2]));
+
+            // Prevent division by zero
+            if (threadCount <= 0) {
+                throw new IllegalArgumentException("Thread count must be greater than zero.");
+            }
+
+            int iters = totalIters / threadCount;
+
+            for (int i = 0; i < 2; i++) {
+                run(lockClass, threadCount, iters);
+            }
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | InterruptedException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
         }
     }
 
-    private static void run(String lockClass, int threadCount, int iters) throws InterruptedException, ClassNotFoundException, IllegalAccessException, InstantiationException {
-        final Counter counter = new SharedCounter(0, (Lock)Class.forName("edu.vt.ece.locks." + lockClass).newInstance());
+    private static void run(String lockClass, int threadCount, int iters)
+            throws InterruptedException, ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        
+        Lock lockInstance;
+
+        if (lockClass.equals(FILTER) || lockClass.equals(BAKERY)) {
+            Class<?> lockClassObj = Class.forName("edu.vt.ece.locks." + lockClass);
+            Constructor<?> lockConstructor = lockClassObj.getDeclaredConstructor(int.class);
+            lockInstance = (Lock) lockConstructor.newInstance(threadCount);
+        } else {
+            lockInstance = (Lock) Class.forName("edu.vt.ece.locks." + lockClass).getDeclaredConstructor().newInstance();
+        }
+
+        final Counter counter = new SharedCounter(0, lockInstance);
         final TestThread2[] threads = new TestThread2[threadCount];
         TestThread2.reset();
 
-        for(int t=0; t<threadCount; t++) {
+        for (int t = 0; t < threadCount; t++) {
             threads[t] = new TestThread2(counter, iters);
         }
 
-        for(int t=0; t<threadCount; t++) {
+        for (int t = 0; t < threadCount; t++) {
             threads[t].start();
         }
 
         long totalTime = 0;
-        for(int t=0; t<threadCount; t++) {
+        for (int t = 0; t < threadCount; t++) {
             threads[t].join();
             totalTime += threads[t].getElapsedTime();
         }
 
-        System.out.println("Average time per thread is " + totalTime/threadCount + "ms");
+        System.out.println("Average time per thread is " + totalTime / threadCount + "ms");
     }
 }
